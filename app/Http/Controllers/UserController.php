76197653d34a977fda9757e623c9017e32474e3b
bellembun <bellyembun69@gmail.com>
2024-06-13 12:44:51 +0700

    bagian update

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,10 +1,48 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\User;
+Use Illuminate\Support\Facades\Hash;
 
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function edit($id)
+    {
+        $user = User::findOrFail($id);
+        return view('user.edit', compact('user'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $rules = [
+            'name' => 'required',
+            'email' => 'required',
+            'password' => 'required',
+        ];
+
+        $message = [
+            'required' => 'Data wajib diisi!',
+            'unique' => 'Data sudah ada!',
+        ];
+
+        // $validation = Validator::make($request->all(), $rules, $message);
+        // if ($validation->fails()) {
+        //     session()->put(
+        //         'danger',
+        //         'Data yang anda input tidak valid, silahkan di ulang'
+        //     );
+        //     return back()
+        //         ->withErrors($validation)
+        //         ->withInput();
+        // }
+        $user = User::findOrFail($id);
+        $user->email = $request->email;
+        $user->name = $request->name;
+        $user->password = Hash::make( $request->password);
+        $user->save();
+        session()->put('success', 'Data Berhasil diedit');
+        return redirect()->route('user.index');
+    }
 }
