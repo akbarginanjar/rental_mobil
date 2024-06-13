641280892d46db7e4cfeafde0285ca4bb26b4879
muhamadzaki <mhmdzaki213@gmail.com>
2024-06-13 12:58:37 +0700

    bagian destroy

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,5 +6,11 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function destroy($id)
+    {
+        $user = User::findOrFail($id);
+        $user->delete();
+        session()->put('success', 'Data Berhasil dihapus');
+        return redirect()->route('user.index');
+    }
 }
