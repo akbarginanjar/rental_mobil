b4b00e939e91c20d83dc4cebe861f678bca1a896
akbarginanjar <akbarginanjarr0@gmail.com>
2024-07-07 20:20:58 +0700

    sidebar

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -14,13 +14,17 @@
     <title>Admin Rental Mobil</title>
     <link rel="canonical" href="https://www.wrappixel.com/templates/Flexy-admin-lite/" />
     <!-- Favicon icon -->
-    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('assets/admin/assets/images/favicon.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('images/icon-moobil.png') }}">
     <!-- Custom CSS -->
     <link href="{{ asset('assets/admin/assets/libs/chartist/dist/chartist.min.css') }}" rel="stylesheet">
     <link href="{{ asset('assets/admin/assets/libs/chartist-plugin-tooltips/dist/chartist-plugin-tooltip.css') }}"
         rel="stylesheet">
     <!-- Custom CSS -->
     <link href="{{ asset('assets/admin/dist/css/style.min.css') }}" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
+        integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
--- resources/views/layouts/partials/admin/sidebar.blade.php
+++ resources/views/layouts/partials/admin/sidebar.blade.php
@@ -10,11 +10,19 @@
                             class="hide-menu">Dashboard</span></a></li>
                 <li class="sidebar-item"> <a
                         class="sidebar-link waves-effect waves-dark sidebar-link {{ Request::is('admin/mobil') ? 'bg-primary' : '' }}"
-                        href="/admin/mobil" aria-expanded="false"><i class="mdi mdi-view-dashboard"></i><span
+                        href="/admin/mobil" aria-expanded="false"><i class="fa-solid fa-car me-2"></i><span
                             class="hide-menu">Mobil</span></a></li>
                 <li class="sidebar-item"> <a
+                        class="sidebar-link waves-effect waves-dark sidebar-link {{ Request::is('admin/transaksi') ? 'bg-primary' : '' }}"
+                        href="/admin/transaksi" aria-expanded="false"><i class="fa-solid fa-receipt me-2"></i><span
+                            class="hide-menu">Transaksi</span></a></li>
+                <li class="sidebar-item"> <a
+                        class="sidebar-link waves-effect waves-dark sidebar-link {{ Request::is('admin/penyewa') ? 'bg-primary' : '' }}"
+                        href="/admin/penyewa" aria-expanded="false"><i class="fa-solid fa-address-card me-2"></i><span
+                            class="hide-menu">Penyewa</span></a></li>
+                <li class="sidebar-item"> <a
                         class="sidebar-link waves-effect waves-dark sidebar-link {{ Request::is('admin/user') ? 'bg-primary' : '' }}"
-                        href="/admin/user" aria-expanded="false"><i class="mdi mdi-view-dashboard"></i><span
+                        href="/admin/user" aria-expanded="false"><i class="fa-solid fa-user me-2"></i><span
                             class="hide-menu">Users</span></a></li>
                 {{-- <li class="text-center p-40 upgrade-btn">
                     <a href="https://www.wrappixel.com/templates/flexy-bootstrap-admin-template/"
