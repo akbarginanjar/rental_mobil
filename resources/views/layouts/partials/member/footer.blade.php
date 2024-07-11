48e89ef244652973adcd137c23d5161986f755f8
Regimuhammadsirozudin <regimuhammad753@gmail.com>
2024-07-11 13:57:42 +0700

    Regi

--- resources/views/layouts/partials/member/footer.blade.php
+++ resources/views/layouts/partials/member/footer.blade.php
@@ -7,8 +7,7 @@
                                 src="{{ asset('images/icon-moobil-erase.png') }}" class="mr-2" width="50px;"
                                 alt="">Rent
                             <span>Moobil</span></a></h2>
-                    <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there
-                        live the blind texts.</p>
+                    <p>Perusahaan rental mobil, menyediakan berbagai jenis dan tipe mobil yang bisa anda kendarai di Kota Bandung.</p>
                     <ul class="ftco-footer-social list-unstyled float-md-left float-lft mt-5">
                         <li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>
                         <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
@@ -18,39 +17,24 @@
             </div>
             <div class="col-md">
                 <div class="ftco-footer-widget mb-4 ml-md-5">
-                    <h2 class="ftco-heading-2">Information</h2>
+                    <h2 class="ftco-heading-2">Menu</h2>
                     <ul class="list-unstyled">
-                        <li><a href="#" class="py-2 d-block">About</a></li>
-                        <li><a href="#" class="py-2 d-block">Services</a></li>
-                        <li><a href="#" class="py-2 d-block">Term and Conditions</a></li>
-                        <li><a href="#" class="py-2 d-block">Best Price Guarantee</a></li>
-                        <li><a href="#" class="py-2 d-block">Privacy &amp; Cookies Policy</a></li>
+                        <li><a href="#" class="py-2 d-block"></a>Beranda</li>
+                        <li><a href="#" class="py-2 d-block">Mobil</a></li>
+                        <li><a href="#" class="py-2 d-block">Tentang Kami</a></li>
+                        
                     </ul>
                 </div>
             </div>
             <div class="col-md">
                 <div class="ftco-footer-widget mb-4">
-                    <h2 class="ftco-heading-2">Customer Support</h2>
-                    <ul class="list-unstyled">
-                        <li><a href="#" class="py-2 d-block">FAQ</a></li>
-                        <li><a href="#" class="py-2 d-block">Payment Option</a></li>
-                        <li><a href="#" class="py-2 d-block">Booking Tips</a></li>
-                        <li><a href="#" class="py-2 d-block">How it works</a></li>
-                        <li><a href="#" class="py-2 d-block">Contact Us</a></li>
-                    </ul>
-                </div>
-            </div>
-            <div class="col-md">
-                <div class="ftco-footer-widget mb-4">
-                    <h2 class="ftco-heading-2">Have a Questions?</h2>
+                    <h2 class="ftco-heading-2">Informasi</h2>
                     <div class="block-23 mb-3">
                         <ul>
-                            <li><span class="icon icon-map-marker"></span><span class="text">203 Fake St. Mountain
-                                    View, San Francisco, California, USA</span></li>
-                            <li><a href="#"><span class="icon icon-phone"></span><span class="text">+2 392 3929
-                                        210</span></a></li>
+                            <li><span class="icon icon-map-marker"></span><span class="text">Jl. soekrno Hatta.Leuwipanjang, Kota Bandung</span></li>
+                            <li><a href="#"><span class="icon icon-phone"></span><span class="text">+6285228564331</span></a></li>
                             <li><a href="#"><span class="icon icon-envelope"></span><span
-                                        class="text">info@yourdomain.com</span></a></li>
+                                        class="text">Rentmoobil@gmail.com</span></a></li>
                         </ul>
                     </div>
                 </div>
@@ -64,7 +48,7 @@
                     <script>
                         document.write(new Date().getFullYear());
                     </script> All rights reserved | This is made with <i class="icon-heart text-danger"
-                        aria-hidden="true"></i> by <a href="/" target="_blank">Kelompok 6</a>
+                        aria-hidden="true"></i> by <a href="/" target="_blank">Kelompok 5</a>
                     <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
                 </p>
             </div>
